79bd7ec32675a718b0b20083d87634e301768327
Tamir Abutbul <1tamir198@gmail.com>
2018-02-01 20:53:37 +0200

    randomize

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 var cards = ["bulbasaur", "charmander", "egg", "greatball", "masterball", "meowth", "pikachu", "pokeball", "premierball", "snorlax", "squirtle", "wigglypuff"]
 
+
+
 function render() {
     var out = document.getElementById("out")
     for (var i = 0; i < cards.length; i++) {
@@ -9,6 +11,51 @@ function render() {
     }
 }
 
-function flip(i){
-   document.getElementById(i).src = `pokemon/${cards[i]}.png`
+function flip(i) {
+    document.getElementById(i).src = `pokemon/${cards[i]}.png`
+}
+
+
+var randCards = []
+
+function randomize() {
+    randCards = []
+    const numOfPairs = 6
+    var mSet = new Set()
+    while (mSet.size < numOfPairs) {
+        mSet.add(Math.floor(Math.random() * cards.length)) //TODO function
+    }
+
+    mSet.forEach(e => { //es6
+        randCards.push(e)
+        randCards.push(e)
+    })
+
+    //the most efficient: Fisher-Yates
+    for(var i = 0; i < randCards.length; i++){
+        var rand = Math.floor(Math.random() * randCards.length)
+
+        var c = randCards.splice(rand, 1)[0];
+        randCards.push(c);
+    }
+}
+
+
+function removeItemSplice(){
+    var fruits = ["Banana", "Orange", "Apple", "Mango"];
+    //we want to remove the "Orange"
+    //orange index is 1 -> start from 1
+    var o = fruits.splice(1/*start*/, 1/*deleteCount*/)
+  
+    document.getElementById("out").innerHTML = o;
+    document.getElementById("out").innerHTML += "<br>" +  fruits;
+}
+
+function addItemSplice(){
+    var fruits = ["Banana", "Orange", "Apple", "Mango"];
+    //we want to remove the "Orange"
+    //orange index is 1 -> start from 1
+     fruits.splice(4/*start*/, 10/*deleteCount*/, "Kiwi") //add the kiwi to the end
+ 
+    document.getElementById("out").innerHTML += "<br>" +  fruits;
 }

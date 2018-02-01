79455ea2066202e350039c62f90c8301914757b3
Tamir Abutbul <1tamir198@gmail.com>
2018-02-01 19:39:53 +0200

    tech

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
-function cards(){
-    alert("")
+var cards = ["bulbasaur", "charmander", "egg", "greatball", "masterball", "meowth", "pikachu", "pokeball", "premierball", "snorlax", "squirtle", "wigglypuff"]
+
+function render() {
+    var out = document.getElementById("out")
+    for (var i = 0; i < cards.length; i++) {
+        out.innerHTML += `<img height="200" class="col-3 card"
+                          id="${i}" onclick="flip(${i})"
+                           src="pokemon/back.png">`
+    }
+}
+
+function flip(i){
+   document.getElementById(i).src = `pokemon/${cards[i]}.png`
 }
